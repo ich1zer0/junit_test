65e982cd0c5657b36876c33cc523e44777d16826
ich1zer0 <proton.ich1@protonmail.com>
2025-03-22 08:48:34 +0900

    テストするファイルアップロード

---
+++ src/main/java/org/example/Check.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Check {
+    private String unuse = "unuse";
+
+        public String evenOrOdd(int num) {
+        if (num % 2 == 0) {
+            return "even";
+        } else {
+            return "odd";
+        }
+    }
+
+    public void main(String[] args) {
+        Calc calc = new Calc();
+        System.out.println(calc.evenOrOdd(2));
+    }
+}
